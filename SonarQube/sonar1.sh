3c1c89155ecf7d965e413198208bcfce6256d572
Wilson Mar <wilsonmar+github@gmail.com>
2017-10-23 09:13:34 -0600

    Update sonar1.sh

--- SonarQube/sonar1.sh
+++ SonarQube/sonar1.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # sonar1.sh From https://github.com/wilsonmar/Dockerfiles/
-# Here is an example (change the sources with the path to your source code; change login token to your SonarQube instance).
+# Uses download from https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner
+# Here is an example for use on a mac.
+# (change: IP address; sources with the path to your source code; login token to your SonarQube instance).
 chmod 555 sonar-scanner
 ./sonar-scanner \
   -Dsonar.projectKey=Angular-35.202.3.232 \
