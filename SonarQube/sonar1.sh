75c33b671a67d941ed643e5b464b78e4f8ed99c4
Wilson Mar <wilsonmar+github@gmail.com>
2017-10-24 08:33:13 -0700

    Update sonar1.sh

--- SonarQube/sonar1.sh
+++ SonarQube/sonar1.sh
@@ -6,6 +6,6 @@
 chmod 555 sonar-scanner
 ./sonar-scanner \
   -Dsonar.projectKey=sonarqube-1-vm \
-  -Dsonar.sources=/Users/wilsonmar/gits/ng \
+  -Dsonar.sources=/Users/wilsonmar/gits/ng/angular4-docker-example \
   -Dsonar.host.url=http://23.236.48.147  \
   -Dsonar.login=7a51cb71a48ea3d16f57fe66021867fc2a98771e
