ed47f25d41d31795512b6840c83290b9c8e0389a
Wilson Mar <wilsonmar+github@gmail.com>
2017-12-01 10:31:53 +0000

    Update hello-world-setup.sh

--- NodeJs/hello-world-setup.sh
+++ NodeJs/hello-world-setup.sh
@@ -4,7 +4,9 @@
 # Install curl if it's not already:
 # sudo apt-get install curl
 
-# Download Dockerfile
-curl -o simple-server.js https://raw.githubusercontent.com/wilsonmar/Dockerfiles/master/NodeJs/Dockerfile
+# Download Dockerfile:
+curl -o https://raw.githubusercontent.com/wilsonmar/Dockerfiles/master/NodeJs/Dockerfile
 
+# Build "hello-node" image in Google Code Repository:
 docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/hello-node:v1 .
+
