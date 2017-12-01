83ed7fbe6c920ca0a98d0d75ca323271125f2559
Wilson Mar <wilsonmar+github@gmail.com>
2017-12-01 10:43:45 +0000

    Update gcp-set-zone.sh

--- gcp-set-zone.sh
+++ gcp-set-zone.sh
@@ -1,9 +1,12 @@
 # This sets the default zone on Google Compute Cloud.
 # Run this with command:
-# bash <(curl -O https://raw.githubusercontent.com/wilsonmar/Dockerfiles/master/gcp-set-zone.sh)
+# bash <(curl -s https://raw.githubusercontent.com/wilsonmar/Dockerfiles/master/gcp-set-zone.sh)
 # Described in https://wilsonmar.github.io/gcp
+# This needs to avoid errors when running kubctl commands. such as:
+#   ERROR: (gcloud.container.clusters.create) One of [--zone] must be supplied: Please specify zone.
 # Environment variables are set so other scripts can determine what zone and region was set.
-# The variables were in Google documentation, so I don't what side affects there may be.
+# This sets a property defined in Google documentation, so I don't what side affects there may be.
+# This is used until I can figure out how to access the property in bash scripts.
 
    export CLOUDSDK_COMPUTE_ZONE=us-central1-f
    export CLOUDSDK_COMPUTE_REGION=us-central1 
